70ab66155ba6825cda1f5563f8316107173738fd
Ray Wang <red.ding.wang@gmail.com>
2018-12-16 23:17:53 +0800

    update skill, note, project

--- js/index.js
+++ js/index.js
@@ -1,15 +1,20 @@
 // smooth scroll
 $(document).ready(function(){
-    $(".scrollIndex").click(function( event ) {
+    $("html,body").hide(0).delay(500).fadeIn(2000);
+
+    $(".scrollIndex").click(function(event){
         event.preventDefault();
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
     });
-      $(".scrollPorfolio").click(function( event ) {
+
+    $(".scrollPorfolio").click(function(event){
         event.preventDefault();
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
     });
-      $(".scrollAbout").click(function( event ) {
+    
+    $(".scrollAbout").click(function(event){
         event.preventDefault();
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
     });
+    
 });
