481abfb95b017da8066052bfa9883378884083ac
ray <ray@rayde-MacBook-Pro.local>
2018-12-28 21:07:42 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+var gulp = require('gulp')
+var jade = require('gulp-jade');
+var sass = require('gulp-sass');
+
+
+gulp.task('copyHTML', function(){
+  return gulp.src('./source/**/*.html')
+    .pipe(gulp.dest('./public'))
+});
+
+
+gulp.task('jade', function() {
+ 
+  gulp.src('./source/**/*.jade')
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./public/'))
+});
+
+
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./source/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
