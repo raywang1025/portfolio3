3d9b2606797134e7d99043fd223cf01c9f00f78a
ray <ray@rayde-MacBook-Pro.local>
2019-01-06 14:08:48 +0800

    add mixpanel click event

--- js/index.js
+++ js/index.js
@@ -16,5 +16,11 @@ $(document).ready(function(){
         event.preventDefault();
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
     });
-    
+
+    window.addEventListener("load", function(event) {
+        var myPortfolio = document.querySelector('.myPortfolio');
+        myPortfolio.addEventListener('click',function(e){
+          e.preventDefault();
+          mixpanel.track("myPortfolio");
+    });
 });
