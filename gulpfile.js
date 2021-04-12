896a7ca0ca1850f884552e8f912c07f358d2f152
Ray Wang <raydingwang@gmail.com>
2021-04-12 10:34:44 +0800

    change layout

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,31 @@ var gulp = require('gulp')
 var jade = require('gulp-jade');
 var sass = require('gulp-sass');
 
-
-gulp.task('copyHTML', function(){
-  return gulp.src('./source/**/*.html')
-    .pipe(gulp.dest('./public'))
+// html in source compile to public
+gulp.task('copyHTML', function() {
+    return gulp.src('./source/**/*.html')
+        .pipe(gulp.dest('./public'))
 });
 
 
 gulp.task('jade', function() {
- 
-  gulp.src('./source/**/*.jade')
-    .pipe(jade({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./public/'))
+
+    gulp.src('./source/**/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./public/'))
 });
 
 
 sass.compiler = require('node-sass');
- 
-gulp.task('sass', function () {
-  return gulp.src('./source/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+
+gulp.task('sass', function() {
+    return gulp.src('./source/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
 });
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+
+gulp.task('sass:watch', function() {
+    gulp.watch('./sass/**/*.scss', ['sass']);
 });
